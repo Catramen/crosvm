588817f3df0e594772a00ab332fdb8352adac297
Dylan Reid <dgreid@chromium.org>
2017-12-22 17:05:00 -0800

    switch to kernel loader
    Change-Id: I20c49b1ae5861f5dc52935f94ab8edc6d3274d0f

---
+++ fuzz/src/fuzz_kernel_loader.rs
@@ -0,0 +1,35 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Fuzzes the kernel loader
+
+extern crate kernel_loader;
+extern crate libc;
+extern crate sys_util;
+
+use sys_util::{GuestAddress, GuestMemory};
+
+use std::io::Cursor;
+use std::slice;
+
+
+fn fuzz_kernel_loader(data: &[u8]) {
+    let rdr = Cursor::new(data);
+    let mem = GuestMemory::new(&[GuestAddress(0), data.len() + 0x1000);
+    let _ = kernel_loader::load_kernel(&mem, GuestAddress(0), kimage);
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn LLVMFuzzerTestOneInput(
+    data: *const libc::uint8_t,
+    size: libc::size_t,
+) -> libc::c_int {
+    let data = unsafe {
+        // Safe as long as the caller is trusted not to modify it during this funciton.
+        slice::from_raw_parts(data, size)
+    };
+    fuzz_kernel_loader(data);
+    0
+}
