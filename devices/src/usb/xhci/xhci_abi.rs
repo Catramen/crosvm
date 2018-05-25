a32c1b3300ceee7684e0471e2c7e302ed1458706
Jingkui Wang <jkwang@google.com>
2018-05-25 10:37:56 -0700

    start to do bitfield.
    Change-Id: Ie85a81f90ec2412e3a34cc7b1aa03f4d93da6888

--- devices/src/usb/xhci/xhci_abi.rs
+++ devices/src/usb/xhci/xhci_abi.rs
@@ -74,6 +74,185 @@ const SPCAP_PORT_COUNT_OFFSET: u32 = 8;
 // Fixed size for all TRB types.
 const TRB_SIZE: usize = 16;
 
+pub enum Error {
+    InvalidValue(u8),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        match self {
+            &Error::InvalidValue(val) => write!(f, "Primitive Enum got an invaild value: {}", val),
+        }
+    }
+}
+
+type Result<T> std::result::Result<T, Error>;
+
+pub trait PrimitiveEnum {
+    fn from(u8: val) -> Result<Self>;
+    fn to(&self) -> u8;
+}
+
+pub enum TrbType {
+  Reserved,
+  Normal,
+  SetupStage,
+  DataStage,
+  StatusStage,
+  Isoch,
+  Link,
+  EventData,
+  Noop,
+  EnableSlotCommand,
+  DisableSlotCommand,
+  AddressDeviceCommand,
+  ConfigureEndpointCommand,
+  EvaluateContextCommand,
+  ResetDeviceCommand,
+  NoopCommand,
+  TransferEvent,
+  CommandCompletionEvent,
+  PortStatusChangeEvent,
+}
+
+impl PrimitiveEnum for TrbType {
+    fn from(u8: val) -> Result<Self> {
+        match val {
+            0 => Ok(TrbType::Reserved),
+            1 => Ok(TrbType::Normal),
+            2 => Ok(TrbType::SetupStage),
+            3 => Ok(TrbType::DataStage),
+            4 => Ok(TrbType::StatusStage),
+            5 => Ok(TrbType::Isoch),
+            6 => Ok(TrbType::Link),
+            7 => Ok(TrbType::EventData),
+            8 => Ok(TrbType::Noop),
+            9 => Ok(TrbType::EnableSlotCommand),
+            10 => Ok(TrbType::DisableSlotCommand),
+            11 => Ok(TrbType::AddressDeviceCommand),
+            12 => Ok(TrbType::ConfigureEndpointCommand),
+            13 => Ok(TrbType::EvaluateContextCommand),
+            17 => Ok(TrbType::ResetDeviceCommand),
+            23 => Ok(TrbType::NoopCommand),
+            32 => Ok(TrbType::TransferEvent),
+            33 => Ok(TrbType::CommandCompletionEvent),
+            34 => Ok(TrbType::PortStatusChangeEvent),
+            _ => Err(Error::InvalidValue);
+        }
+    }
+    fn to(&self) -> u8 {
+        match self {
+            &TrbType::Reserved => 0,
+            &TrbType::Normal => 1,
+            &TrbType::SetupStage => 2,
+            &TrbType::DataStage => 3,
+            &TrbType::StatusStage => 4,
+            &TrbType::Isoch => 5,
+            &TrbType::Link => 6,
+            &TrbType::EventData => 7,
+            &TrbType::Noop => 8,
+            &TrbType::EnableSlotCommand => 9,
+            &TrbType::DisableSlotCommand => 10,
+            &TrbType::AddressDeviceCommand => 11,
+            &TrbType::ConfigureEndpointCommand => 12,
+            &TrbType::EvaluateContextCommand => 13,
+            &TrbType::ResetDeviceCommand => 17,
+            &TrbType::NoopCommand => 23,
+            &TrbType::TransferEvent => 32,
+            &TrbType::CommandCompletionEvent => 33,
+            &TrbType::PortStatusChangeEvent => 34,
+        }
+    }
+}
+
+pub enum TrbCompletionCode {
+    Success = 1,
+    TransactionError = 4,
+    TrbError = 5,
+    NoSlotsAvailableError = 9,
+    SlotNotEnabledError = 11,
+    ShortPacket = 13,
+    ContextStateError = 19,
+}
+
+impl PrimitiveEnum for TrbCompletionCode {
+    fn from(u8: val) -> Result<Self> {
+        match val {
+            1 => Ok(TrbCompletionCode::Success),
+            4 => Ok(TrbCompletionCode::TransactionError),
+            5 => Ok(TrbCompletionCode::TrbError),
+            9 => Ok(TrbCompletionCode::NoSlotsAvailableError),
+            11 => Ok(TrbCompletionCode::SlotNotEnabledError),
+            13 => Ok(TrbCompletionCode::ShortPacket),
+            19 => Ok(TrbCompletionCode::ContextStateError),
+            _ => Err(Error::InvalidValue),
+        }
+    }
+
+    fn to(&self) -> u8 {
+        match self {
+            &TrbCompletionCode::Success => 1,
+            &TrbCompletionCode::TransactionError => 4,
+            &TrbCompletionCode::TrbError => 5,
+            &TrbCompletionCode::NoSlotsAvailableError => 9,
+            &TrbCompletionCode::SlotNotEnabledError => 11,
+            &TrbCompletionCode::ShortPacket => 13,
+            &TrbCompletionCode::ContextStateError => 19,
+        }
+    }
+}
+
+pub enum DeviceSlotState {
+    // The same value (0) is used for both the enabled and disabled states. See
+    // xhci spec table 60.
+    DisabledOrEnabled,
+    Default,
+    StateAddressed,
+    Configured,
+};
+
+impl PrimitiveEnum for DeviceSlotState {
+    fn from(u8: val) -> Result<Self> {
+        match val {
+            0 => Ok(DeviceSlotState::DisabledOrEnabled),
+            1 => Ok(DeviceSlotState::Default),
+            2 => Ok(DeviceSlotState::Addressed),
+            3 => Ok(DeviceSlotState::Configured),
+            _ => Err(Error::InvalidValue),
+        }
+    }
+
+    fn to(&self) -> u8 {
+        match self {
+            &DeviceSlotState::DisabledOrEnabled => 0,
+            &DeviceSlotState::Default => 1,
+            &DeviceSlotState::Addressed => 2,
+            &DeviceSlotState::Configured => 3,
+        }
+    }
+}
+
+pub enum EndpointState {
+  Disabled = 0,
+  Running = 1,
+};
+
+impl PrimitiveEnum for EndpointState {
+    fn from(u8: val) -> Result<Self> {
+        match val {
+            0 => Ok(EndpointState::Disabled),
+            1 => Ok(EndpointState::Running),
+            _ => Err(Error::InvalidValue),
+        }
+    }
+
+    fn to(&self) -> u8 {
+        match self {
+            &EndpointState::Disabled => 0,
+            &EndpointState::Running => 1,
+        }
+    }
+}
 #[derive(BitField)]
 #[passthrough(derive(Clone, Copy))]
 
