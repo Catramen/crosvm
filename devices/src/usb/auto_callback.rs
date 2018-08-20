ea173fe196e064fc722a9a3bc50fd6361399759b
Jingkui Wang <jkwang@google.com>
2018-08-20 15:39:40 -0700

    usb: Add autocallback
    We need to handle xhci stop request asynchronously. AutoCallback allow
    some cb invoked when all ring buffer controllers stopped.
    
    BUG=chromium:831850
    TEST=cargo test
    
    Change-Id: I0643dcaae5b151f9514f78cc661bfdd6fa489a78

---
+++ devices/src/usb/auto_callback.rs
@@ -0,0 +1,53 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use std::sync::{Arc, Mutex};
+
+/// AutoCallback wraps a callback. The callback will be invoked when last instance of AutoCallback
+/// and it's clones is dropped.
+#[derive(Clone)]
+pub struct AutoCallback {
+    inner: Arc<Mutex<AutoCallbackInner>>,
+}
+
+impl AutoCallback {
+    pub fn new<C: 'static + Fn() + Send>(cb: C) -> AutoCallback {
+        AutoCallback {
+            inner: Arc::new(Mutex::new(AutoCallbackInner {
+                callback: Box::new(cb),
+            })),
+        }
+    }
+}
+
+struct AutoCallbackInner {
+    callback: Box<Fn() + Send>,
+}
+
+impl Drop for AutoCallbackInner {
+    fn drop(&mut self) {
+        (self.callback)();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::{Arc, Mutex};
+
+    fn task(_: AutoCallback) {}
+
+    #[test]
+    fn simple_raii_callback() {
+        let a = Arc::new(Mutex::new(0));
+        let ac = a.clone();
+        let cb = AutoCallback::new(move || {
+            *ac.lock().unwrap() = 1;
+        });
+        task(cb.clone());
+        task(cb.clone());
+        task(cb);
+        assert_eq!(*a.lock().unwrap(), 1);
+    }
+}
