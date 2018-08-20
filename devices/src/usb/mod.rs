0e5ddb2f3e411c840cececa198493aec6b56a9b9
Jingkui Wang <jkwang@google.com>
2018-08-20 15:39:40 -0700

    usb: Add autocallback
    We need to handle xhci stop request asynchronously. AutoCallback allow
    some cb invoked when all ring buffer controllers stopped.
    
    BUG=chromium:831850
    TEST=cargo test
    
    Change-Id: I0643dcaae5b151f9514f78cc661bfdd6fa489a78

--- devices/src/usb/mod.rs
+++ devices/src/usb/mod.rs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#[allow(dead_code)]
+mod auto_callback;
 mod xhci;
 #[allow(dead_code)]
 mod event_loop;
+
