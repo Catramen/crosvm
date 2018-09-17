f8dc4e99731948c8d223ffab3ab7253fe7435d29
Jingkui Wang <jkwang@google.com>
2018-09-17 11:15:54 -0700

    tmp
    Change-Id: I3946e136daa724c546d6aaac01089ad5db891767

--- devices/src/usb/host_backend/host_device.rs
+++ devices/src/usb/host_backend/host_device.rs
@@ -5,10 +5,10 @@
 use std::sync::{Arc, Mutex};
 
 use usb::xhci::xhci_backend_device::{XhciBackendDevice, UsbDeviceAddress};
-use usb::xhci::xhci_transfer::{XhciTransfer, XhciTransferType, TransferStatus};
+use usb::xhci::xhci_transfer::{XhciTransfer, XhciTransferType};
 use usb_util::device_handle::DeviceHandle;
-use usb_util::usb_transfer::{UsbTransfer, ControlTransferBuffer, control_transfer};
-use usb_util::types::ControlRequestDataPhaseTransferDirection;
+use usb_util::usb_transfer::{UsbTransfer, ControlTransferBuffer, control_transfer, TransferStatus};
+use usb_util::types::{UsbRequestSetup, ControlRequestDataPhaseTransferDirection, ControlRequestType, ControlRequestRecipient, StandardControlRequest};
 use super::usb_endpoint::UsbEndpoint;
 
 #[derive(PartialEq)]
@@ -51,14 +51,14 @@ impl HostDevice {
             panic!("Last control transfer has not yet finished.");
         }
         match transfer.get_transfer_type() {
-            XhciTransferType::SetupStage(ref setup) => {
+            XhciTransferType::SetupStage(setup) => {
                 if (*self.ctl_ep_state.lock().unwrap()) != ControlEndpointState::StatusStage {
                     error!("Control endpoing is in an inconsistant state");
                     return;
                 }
                 let mut locked = self.control_transfer.lock().unwrap();
                 {
-                    let request_setup = &mut locked.as_mut().unwrap().buffer().setup_buffer;
+                    let request_setup = &mut locked.as_mut().unwrap().mut_buffer().setup_buffer;
                     *request_setup = setup.clone();
                 }
                 // If the control transfer is device to host, we can go ahead submit the
@@ -68,34 +68,38 @@ impl HostDevice {
                         let mut control_transfer = locked.take().unwrap();
                         let myct = self.control_transfer.clone();
                         control_transfer.set_callback(move |t: UsbTransfer<ControlTransferBuffer>| {
+                            let status = t.status();
+                            let actual_length = t.actual_length();
                             (*myct.lock().unwrap()) = Some(t);
-                            // TODO(jkwang) transfer completed. How is that?
-                            //transfer.on_transfer_complete();
+                            transfer.on_transfer_complete(status, actual_length as u32);
                         });
                         self.device_handle.lock().unwrap().submit_async_transfer(control_transfer);
                 } else {
-                    // TODO(jkwang) really completed.
-                    //transfer.on_transfer_complete();
+                    transfer.on_transfer_complete(TransferStatus::Completed, 0);
                 }
                 *self.ctl_ep_state.lock().unwrap() = ControlEndpointState::SetupStage;
             },
-            XhciTransferType::DataStage(ref buffer) => {
+            XhciTransferType::DataStage(buffer) => {
                  if (*self.ctl_ep_state.lock().unwrap()) != ControlEndpointState::SetupStage {
                     error!("Control endpoing is in an inconsistant state");
                     return;
                  }
                  let mut locked = self.control_transfer.lock().unwrap();
                  let control_transfer = locked.as_mut().unwrap();
-                 let tbuffer = control_transfer.buffer();
+                 let tbuffer = control_transfer.mut_buffer();
                  let request_setup = &tbuffer.setup_buffer;
                  match request_setup.get_direction() {
                          Some(ControlRequestDataPhaseTransferDirection::HostToDevice) => {
+                             // Read from dma to host buffer.
+                             let bytes = buffer.read(&mut tbuffer.data_buffer) as u32;
+                             transfer.on_transfer_complete(TransferStatus::Completed, bytes);
                          },
                          Some(ControlRequestDataPhaseTransferDirection::DeviceToHost) => {
                              // For device to host transfer, it's already handled in setup stage.
                              // As ScatterGatherBuffer implementation handles buffer size, we can
                              // copy all buffer.
-                             buffer.write(&tbuffer.data_buffer);
+                             let bytes = buffer.write(&tbuffer.data_buffer) as u32;
+                             transfer.on_transfer_complete(TransferStatus::Completed, bytes);
                          },
                          _ => error!("Unknown transfer direction!"),
                  }
@@ -108,6 +112,37 @@ impl HostDevice {
                     error!("Control endpoing is in an inconsistant state");
                     return;
                 }
+                let mut locked = self.control_transfer.lock().unwrap();
+                let request_setup = {
+                    let control_transfer = locked.as_mut().unwrap();
+                    let mut tbuffer = control_transfer.buffer();
+                    tbuffer.setup_buffer.clone()
+                };
+                match request_setup.get_direction() {
+                    Some(ControlRequestDataPhaseTransferDirection::HostToDevice) => {
+                        // We handle some standard request with libusb api.
+                        let s = self.handle_standard_control_requested(&request_setup);
+                        if s.is_some() {
+                            transfer.on_transfer_complete(s.unwrap(), 0);
+                            return;
+                        }
+                        let mut control_transfer = locked.take().unwrap();
+                        let myct = self.control_transfer.clone();
+                        control_transfer.set_callback(move |t: UsbTransfer<ControlTransferBuffer>| {
+                            let status = t.status();
+                            let actual_length = t.actual_length();
+                            (*myct.lock().unwrap()) = Some(t);
+                            transfer.on_transfer_complete(status, 0);
+                        });
+                        self.device_handle.lock().unwrap().submit_async_transfer(control_transfer);
+
+                    },
+                    Some(ControlRequestDataPhaseTransferDirection::DeviceToHost) => {
+                        transfer.on_transfer_complete(TransferStatus::Completed, 0);
+                    },
+                    _ => error!("Unknown transfer direction!"),
+                }
+
                 *self.ctl_ep_state.lock().unwrap() = ControlEndpointState::StatusStage;
             },
             _ => {
@@ -116,13 +151,87 @@ impl HostDevice {
         }
     }
 
-    fn set_config(&self) {
+    fn handle_standard_control_requested(&self, request_setup: &UsbRequestSetup)
+        -> Option<TransferStatus> {
+        let s = self.set_address_if_requested(request_setup);
+        if s.is_some() {
+            return s;
+        };
+        let s = self.set_config_if_requested(request_setup);
+        if s.is_some() {
+            return s;
+        };
+        let s = self.set_interface_if_requested(request_setup);
+        if s.is_some() {
+            return s;
+        };
+        let s = self.clear_feature_if_requested(request_setup);
+        if s.is_some() {
+            return s;
+        };
+        None
     }
 
-    fn release_interface(&self) {
+    fn set_address_if_requested(&self, request_setup: &UsbRequestSetup) -> Option<TransferStatus> {
+        if request_setup.get_type().unwrap() != ControlRequestType::Standard ||
+            request_setup.get_recipient() != ControlRequestRecipient::Device ||
+                request_setup.get_standard_request().unwrap() != StandardControlRequest::SetAddress {
+                    return None;
+        }
+        // It's a standard, set_address, device request. We do nothing here.
+        debug!("Set address control transfer is received with address: {}", request_setup.value);
+        Some(TransferStatus::Completed)
     }
 
-    fn set_interface(&self) {
+    fn set_config_if_requested(&self, request_setup: &UsbRequestSetup) -> Option<TransferStatus> {
+        if request_setup.get_type().unwrap() != ControlRequestType::Standard ||
+            request_setup.get_recipient() != ControlRequestRecipient::Device ||
+                request_setup.get_standard_request().unwrap() != StandardControlRequest::SetConfiguration {
+                    return None;
+        }
+        // It's a standard, set_config, device request.
+        let config = (request_setup.value & 0xff) as i32;
+        debug!("Set config control transfer is received with config: {}", config);
+        self.release_interfaces();
+        let cur_config = self.device_handle.lock().unwrap().get_active_configuration().unwrap();
+        debug!("Cur config is: {}", cur_config);
+        if config != cur_config {
+            self.device_handle.lock().unwrap().set_active_configuration(config).unwrap();
+        }
+        self.claim_interfaces();
+        self.create_endpoints();
+        Some(TransferStatus::Completed)
+    }
+
+    fn set_interface_if_requested(&self, request_setup: &UsbRequestSetup) -> Option<TransferStatus> {
+        if request_setup.get_type().unwrap() != ControlRequestType::Standard ||
+            request_setup.get_recipient() != ControlRequestRecipient::Interface ||
+                request_setup.get_standard_request().unwrap() != StandardControlRequest::SetInterface {
+                    return None;
+        }
+        // It's a standard, set_interface, interface request.
+        Some(TransferStatus::Completed)
+    }
+
+    fn clear_feature_if_requested(&self, request_setup: &UsbRequestSetup) -> Option<TransferStatus> {
+        if request_setup.get_type().unwrap() != ControlRequestType::Standard ||
+            request_setup.get_recipient() != ControlRequestRecipient::Endpoint ||
+                request_setup.get_standard_request().unwrap() != StandardControlRequest::ClearFeature {
+                    return None;
+                }
+        // It's a standard, clear_feature, endpoint request.
+
+        Some(TransferStatus::Completed)
+    }
+
+    fn release_interfaces(&self) {
+
+    }
+
+    fn claim_interfaces(&self) {
+    }
+
+    fn create_endpoints(&self) {
     }
 
     fn delete_all_endpoints(&mut self) {
@@ -137,7 +246,7 @@ impl XhciBackendDevice for HostDevice {
             return;
         }
         for ep in &self.endpoints {
-            if ep.match_ep(transfer.get_endpoint_number(), transfer.get_endpoint_dir()) {
+            if ep.match_ep(transfer.get_endpoint_number(), &transfer.get_endpoint_dir()) {
                 ep.handle_transfer(transfer);
                 return;
             }
@@ -147,5 +256,6 @@ impl XhciBackendDevice for HostDevice {
     }
 
     fn set_address(&self, address: UsbDeviceAddress) {
+        debug!("Set address is invoked with address: {}", address);
     }
 }
--- devices/src/usb/host_backend/usb_endpoint.rs
+++ devices/src/usb/host_backend/usb_endpoint.rs
@@ -4,9 +4,11 @@
 
 use std::sync::{Arc, Mutex};
 
-use usb::xhci::xhci_transfer::{XhciTransfer, EndpointDirection};
-use usb_util::types::EndpointType;
+use usb::xhci::xhci_transfer::{XhciTransfer, EndpointDirection, XhciTransferType};
+use usb::xhci::scatter_gather_buffer::ScatterGatherBuffer;
+use usb_util::types::{EndpointType};
 use usb_util::device_handle::DeviceHandle;
+use usb_util::usb_transfer::{UsbTransfer, BulkTransferBuffer, TransferStatus, bulk_transfer};
 
 pub struct UsbEndpoint {
     device_handle: Arc<Mutex<DeviceHandle>>,
@@ -34,5 +36,55 @@ impl UsbEndpoint {
     }
 
     pub fn handle_transfer(&self, transfer: XhciTransfer) {
+        if self.ty != EndpointType::Bulk {
+            warn!("Endpoint type is not supporetd");
+            transfer.on_transfer_complete(TransferStatus::Error, 0);
+        }
+
+        match transfer.get_transfer_type() {
+            XhciTransferType::Normal(buffer) => {
+                self.handle_bulk_transfer(transfer, buffer);
+            },
+            _ => {
+                error!("Wrong transfer type routed to Bulk endpoint.");
+
+            },
+        }
+    }
+
+    fn handle_bulk_transfer(&self, transfer: XhciTransfer, buffer: ScatterGatherBuffer) {
+        let mut bulk_transfer = bulk_transfer(self.endpoint_number, 0, buffer.len());
+        match self.direction {
+            EndpointDirection::In => {
+                // Read data from ScatterGatherBuffer to a continuous memory.
+                buffer.read(bulk_transfer.mut_buffer().mut_slice());
+                bulk_transfer.set_callback(move |t: UsbTransfer<BulkTransferBuffer>| {
+                    let status = t.status();
+                    let actual_length = t.actual_length();
+                    transfer.on_transfer_complete(status, actual_length as u32);
+                });
+                self.device_handle.lock().unwrap().submit_async_transfer(bulk_transfer);
+            },
+            EndpointDirection::Out => {
+                bulk_transfer.set_callback(move |t: UsbTransfer<BulkTransferBuffer>| {
+                    let status = t.status();
+                    let actual_length = t.actual_length() as usize;
+                    let copied_length = buffer.write(t.buffer().slice());
+                    let actual_length = {
+                        if actual_length > copied_length {
+                            copied_length
+                        } else {
+                            actual_length
+                        }
+                    };
+                    transfer.on_transfer_complete(status, actual_length as u32);
+                });
+                self.device_handle.lock().unwrap().submit_async_transfer(bulk_transfer);
+            },
+            _ => {
+                error!("Wrong direction for bulk endpoint");
+                transfer.on_transfer_complete(TransferStatus::Error, 0);
+            }
+        }
     }
 }
