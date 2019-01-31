11c15a377d729620e0383c6bc24e19faee36c49c
Jingkui Wang <jkwang@google.com>
2019-01-30 21:11:52 -0800

    bindings
    Change-Id: I5f0d17387301db1cff422906756682b6aee5d39c

---
+++ usbdev/src/lib.rs
@@ -0,0 +1,20 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Generated with bindgen libusb.h -no-prepend-enum-name -o bindings.rs.
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#[cfg_attr(feature = "cargo-clippy", allow(clippy))]
+mod bindings;
+
+extern crate assertions;
+extern crate data_model;
+extern crate sync;
+
+#[macro_use]
+mod error;
+
+pub use error::*;
